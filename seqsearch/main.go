0c0b75fd977d2f5fcf565b5aa14813e53707d988
51cat <lzh1996035@163.com>
2024-04-02 16:39:00 +0800

    增加命令行接口

--- seqsearch/main.go
+++ seqsearch/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	//"fmt"
+	"flag"
 	"seqsearch/utils"
 )
 
 func main() {
+
+	var input string
+	var format string
+	var target_fa string
+	var mode string
+	var out string
+	var sample_name string
+
+	var mistmatch int
+	var start int
+	var end int
+
+	flag.StringVar(&input, "input", "", "Input Your fasta/fastq path")
+	flag.StringVar(&format, "format", "", "fastq/fasta")
+	flag.StringVar(&mode, "mode", "first", "all/first")
+	flag.StringVar(&target_fa, "target_fa", "", "Target fasta path")
+	flag.StringVar(&out, "out", "./count_out.tsv", "path of out file")
+	flag.StringVar(&sample_name, "sample_name", "sample", "sample name")
+
+	flag.IntVar(&mistmatch, "mistmatch", 0, "mismatch")
+	flag.IntVar(&start, "start",0 , "search region: start")
+	flag.IntVar(&end, "end", 99999, "search region: end")
+	flag.Parse()
+
 	res:=utils.Search(
-		"../test_data/test.fastq",
-		"fastq",
-		"../test_data/target.fa",
-		"all",
-		2,
-		0,
-		1000000,
+		input,
+		format,
+		target_fa,
+		mode,
+		mistmatch,
+		start,
+		end,
 	)
 	//fmt.Println(res)
 	//utils.WriteSearchRes(res,"./ANN.tsv")
-	utils.CountSearchRes("CC", res,"./out.tsv")
+	utils.CountSearchRes(sample_name, res, out)
 }
